70faf4531bd3d27d223da6e7b42943b766cae620
nordmograph <nordmograph@users.noreply.github.com>
2016-12-25 22:35:03 +0100

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * @copyright	Copyright (c) 2016 airmap. All rights reserved.
- * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ * Air Quality Map Module 
+ * @link https://www.nordmograph.com/extensions
+ * @copyright Copyright (C) 2017 Adrien Roussel nordmograph.com
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
-
 // no direct access
 defined('_JEXEC') or die;
 JHtml::_('jquery.framework');					
